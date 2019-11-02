cc1c152bfad34e587ebc2e88dbd90bfe4bf04ebf
Sibsankar Bera <sibsankarb4@gmail.com>
2019-11-03 00:40:25 +0530

    Add files via upload

---
+++ src/main/java/com/sibsankar/spring/openshift/api/SpringExampleApplication.java
@@ -0,0 +1,26 @@
+package com.sibsankar.spring.openshift.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExampleApplication {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "++++++++++++++++++++++++++++ Welcome to java Experimental Squard ++++++++++++++++++++++++++++ ";
+	}
+
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "Hello " + input + ", Your application deployed successfully. Cheers...!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+}
